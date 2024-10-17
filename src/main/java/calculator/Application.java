7eca95e353034f881996b0d4b02ef3edd0368672
Dompoo <dlckdrms0517@gmail.com>
2024-10-17 17:46:18 +0900

    feat: Application 구현
    - ApplicationFactory를 통해 의존성 관리
    - Application에서는 ApplicationFactory를 통해 받은 객체들을 사용해 요구사항 달성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.io.InputHandleResult;
+import calculator.io.InputOutputHandler;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationFactory factory = new ApplicationFactory();
+        InputOutputHandler handler = factory.getInputOutputHandler();
+        StringCalculator stringCalculator = factory.getStringCalculator();
+        
+        InputHandleResult input = handler.handleInput();
+        int result = stringCalculator.calculate(input.customSeparatorPart(), input.numberPart());
+        handler.handleOutput(result);
     }
 }

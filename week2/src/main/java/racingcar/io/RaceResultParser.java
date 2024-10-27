65eb1d13b5a765288b462c01b90f3798b0cf4375
이창근 <dlckdrms0517@gmail.com>
2024-10-27 15:44:30 +0900

    feat: RaceResultParser 개발
    - 게임 후의 RaceResult 객체를 문자열로 파싱해줍니다.

---
+++ week2/src/main/java/racingcar/io/RaceResultParser.java
@@ -0,0 +1,51 @@
+package racingcar.io;
+
+import java.util.List;
+import racingcar.dto.CarStatus;
+import racingcar.dto.LapResult;
+import racingcar.dto.RaceResult;
+
+public class RaceResultParser {
+
+    private static final String NEW_LINE = "\n";
+    private static final String EXECUTION_RESULT = "실행 결과 : ";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
+    private static final String WINNER_SEPARATOR = ", ";
+
+    public String parse(RaceResult raceResult) {
+        StringBuilder sb = new StringBuilder();
+
+        appendExecutionResults(sb, raceResult.getLapResults());
+        appendWinners(sb, raceResult.getWinners());
+
+        return sb.toString();
+    }
+
+    private void appendExecutionResults(StringBuilder sb, List<LapResult> lapResults) {
+        sb.append(NEW_LINE)
+                .append(EXECUTION_RESULT)
+                .append(NEW_LINE);
+
+        if (lapResults != null) {
+            for (LapResult result : lapResults) {
+                sb.append(result)
+                        .append(NEW_LINE);
+            }
+        }
+    }
+
+    private void appendWinners(StringBuilder sb, List<CarStatus> winners) {
+        sb.append(FINAL_WINNER);
+
+        if (winners == null || winners.isEmpty()) {
+            return;
+        }
+
+        for (int i = 0; i < winners.size(); i++) {
+            sb.append(winners.get(i).getName());
+            if (i != winners.size() - 1) {
+                sb.append(WINNER_SEPARATOR);
+            }
+        }
+    }
+}

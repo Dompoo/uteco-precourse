e74bc1166233c9a0b45e5f24cea70d34ab41ac91
이창근 <dlckdrms0517@gmail.com>
2024-10-31 00:05:15 +0900

    feat(domain): Lotto 검증 추가
    - 각 숫자가 1~45 사이인지 확인하는 검증 추가

--- week3/src/main/java/lotto/domain/Lotto.java
+++ week3/src/main/java/lotto/domain/Lotto.java
@@ -16,6 +16,16 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ExceptionMessages.LOTTO_NUMBER_NOT_6.message);
         }
+
+        numbers.forEach(number -> {
+            if (!isNumberValid(number)) {
+                throw new IllegalArgumentException(ExceptionMessages.LOTTO_NUMBER_NOT_VALID.message);
+            }
+        });
+    }
+
+    private boolean isNumberValid(Integer number) {
+        return 1 <= number && number <= 45;
     }
 
     // TODO: 추가 기능 구현

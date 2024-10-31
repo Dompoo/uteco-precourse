2f3963d211fdc888a92c861912f455060a22f96a
이창근 <dlckdrms0517@gmail.com>
2024-10-31 20:50:38 +0900

    feat: LottoController, Application 개발

--- week3/src/main/java/lotto/Application.java
+++ week3/src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.domain.numberProvider.RandomNumberPicker;
+import lotto.io.InputHandler;
+import lotto.io.OutputHandler;
+import lotto.io.reader.MissionUtilsReader;
+import lotto.io.writer.SystemWriter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MissionUtilsReader reader = new MissionUtilsReader();
+        SystemWriter writer = new SystemWriter();
+        LottoController lottoController = new LottoController(
+                new InputHandler(reader, writer),
+                new OutputHandler(writer),
+                new RetryHandler(writer),
+                new RandomNumberPicker()
+        );
+
+        lottoController.run();
     }
 }
---
+++ week3/src/main/java/lotto/LottoController.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoStatics;
+import lotto.domain.Money;
+import lotto.domain.WinningLotto;
+import lotto.domain.numberProvider.NumberPicker;
+import lotto.io.InputHandler;
+import lotto.io.OutputHandler;
+
+public class LottoController {
+
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+    private final RetryHandler retryHandler;
+    private final NumberPicker numberPicker;
+
+    public LottoController(
+            InputHandler inputHandler,
+            OutputHandler outputHandler,
+            RetryHandler retryHandler,
+            NumberPicker numberPicker
+    ) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+        this.retryHandler = retryHandler;
+        this.numberPicker = numberPicker;
+    }
+
+    public void run() {
+        Money money = retryHandler.tryUntilSuccess(() -> {
+            int amount = inputHandler.handlePurchaseAmount();
+            return Money.from(amount);
+        });
+
+        List<Lotto> purchasedLottos = Lotto.purchase(money, numberPicker);
+
+        outputHandler.handlePurchasedLottos(purchasedLottos);
+
+        WinningLotto winningLotto = retryHandler.tryUntilSuccess(() -> {
+            List<Integer> numbers = inputHandler.handleWinningLottoNumbers();
+            int bonusNumber = inputHandler.handleWinningLottoBonusNumber();
+            return WinningLotto.of(numbers, bonusNumber);
+        });
+
+        LottoStatics lottoStatics = LottoStatics.of(purchasedLottos, winningLotto, money);
+
+        outputHandler.handlePrizeStatics(lottoStatics.getPrizeStatics());
+        outputHandler.handleIncomeStatics(lottoStatics.getIncomeStatics());
+    }
+}

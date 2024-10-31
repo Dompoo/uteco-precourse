fd0799f311647cd93d2b47c850997302e6f85eea
이창근 <dlckdrms0517@gmail.com>
2024-10-31 14:46:50 +0900

    feat(domain): WinningLotto 정합성 검증 추가
    이제 보너스 숫자와 로또숫자의 중복을 확인합니다.

--- week3/src/main/java/lotto/domain/WinningLotto.java
+++ week3/src/main/java/lotto/domain/WinningLotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.BonusNumberDuplicatedException;
 
 public class WinningLotto {
 
@@ -8,9 +9,15 @@ public class WinningLotto {
     private final Number bonusNumber;
 
     private WinningLotto(Lotto lotto, Number bonusNumber) {
-        // TODO : validation 필요
+        validate(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private static void validate(Lotto lotto, Number bonusNumber) {
+        if (lotto.contains(bonusNumber)) {
+            throw new BonusNumberDuplicatedException();
+        }
     }
 
     public static WinningLotto of(List<Integer> numbers, int bonusNumber) {

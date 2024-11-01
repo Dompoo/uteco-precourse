78948f5ff8ddf2199bd400bb5293bc20b81f61b3
이창근 <dlckdrms0517@gmail.com>
2024-11-02 01:22:09 +0900

    fix: LottoPrize 순서 수정

--- week3/src/main/java/lotto/domain/LottoPrize.java
+++ week3/src/main/java/lotto/domain/LottoPrize.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 public enum LottoPrize {
 
-    FRIST_PRIZE(6, 0, 2000000000, "6개 일치 (2,000,000,000원)"),
-    SECOND_PRIZE(5, 1, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD_PRIZE(5, 0, 1500000, "5개 일치 (1,500,000원)"),
-    FOURTH_PRIZE(4, 0, 50000, "4개 일치 (50,000원)"),
     FIFTH_PRIZE(3, 0, 5000, "3개 일치 (5,000원)"),
+    FOURTH_PRIZE(4, 0, 50000, "4개 일치 (50,000원)"),
+    THIRD_PRIZE(5, 0, 1500000, "5개 일치 (1,500,000원)"),
+    SECOND_PRIZE(5, 1, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FRIST_PRIZE(6, 0, 2000000000, "6개 일치 (2,000,000,000원)"),
     ;
 
     private final int numberMatch;

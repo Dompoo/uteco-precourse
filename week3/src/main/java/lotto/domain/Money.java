f761ac462db39598fec975b46ccc5b2a6d5aeef8
이창근 <dlckdrms0517@gmail.com>
2024-11-02 15:36:43 +0900

    feat(domain): 돈 최대값 검증 추가

--- week3/src/main/java/lotto/domain/Money.java
+++ week3/src/main/java/lotto/domain/Money.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
 import lotto.exception.MoneyAmountLackException;
+import lotto.exception.MoneyAmountOverException;
 import lotto.exception.MoneyUnitInvalidException;
 
 public class Money {
 
     private static final int MIN_MONEY = 1000;
+    private static final int MAX_MONEY = 100000;
     private static final int MONEY_UNIT = 1000;
 
     private final int amount;
@@ -24,10 +26,15 @@ public class Money {
     }
 
     private static void validate(int amount) {
-        if (amount < 1000) {
+        if (amount < MIN_MONEY) {
             throw new MoneyAmountLackException(MIN_MONEY);
         }
-        if (amount % 1000 != 0) {
+
+        if (amount > MAX_MONEY) {
+            throw new MoneyAmountOverException(MAX_MONEY);
+        }
+
+        if (amount % MONEY_UNIT != 0) {
             throw new MoneyUnitInvalidException(MONEY_UNIT);
         }
     }
